e413e0d646c2dc303eec7e7d8c4a207360873529
Syanmil <syanmil@yahoo.com>
2017-03-16 17:27:47 +0700

    initialize project folders

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
+require('dotenv').config()
 
 var index = require('./routes/index');
 var users = require('./routes/users');
